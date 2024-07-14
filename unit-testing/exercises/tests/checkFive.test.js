0f08a8ef05e84a6785ca3121dc4826ad195244f1
Harrison Sayre <hsayre42@gmail.com>
2024-07-14 14:07:01 -0400

    <Did checkFive>

--- unit-testing/exercises/tests/checkFive.test.js
+++ unit-testing/exercises/tests/checkFive.test.js
@@ -1,1 +1,20 @@
-const test = require('../checkFive.js');
+const checkFive = require('../checkFive.js');
+
+describe("checkFive", function(){
+    test("Returns 'number is less than 5.' when passed a number < 5", function(){
+        let output = checkFive(2);
+        expect(output).toBe("2 is less than 5.");
+    });
+
+    test("Returns 'number is greater than 5.' when passed a number > 5", function(){
+        let output = checkFive(6);
+        expect(output).toBe("6 is greater than 5.");
+    });
+
+    test("Returns 'number is equal to 5.' when passed 5", function(){
+        let output = checkFive(5);
+        expect(output).toBe("5 is equal to 5.");
+    });
+    
+
+});
