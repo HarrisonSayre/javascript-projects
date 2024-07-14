81acf255b50483772a0a897a8894014ec34c16f6
Harrison Sayre <hsayre42@gmail.com>
2024-07-14 13:22:52 -0400

    <Did examples>

--- unit-testing/chapter-examples/transmission-processor/tests/processor.test.js
+++ unit-testing/chapter-examples/transmission-processor/tests/processor.test.js
@@ -6,6 +6,52 @@ describe("transmission processor", function() {
   test("takes a string returns an object", function(){
     let result = processor("9701::<489584872710>");
     expect(typeof result).toBe("object");
-   });
- 
+  });
+
+  test("returns -1 if '::' not found", function() {
+    let result = processor("9701<489584872710>");
+    expect(result).toBe(-1);
+  });
+
+  test("returns id in object", function() {
+    let result = processor("9701::<489584872710>");
+    expect(result.id).not.toBeUndefined();
+  });
+  test("converts id to a number", function() {
+      let result = processor("9701::<489584872710>");
+      expect(result.id).toBe(9701);
+  });
+  test("returns rawData in object", function() {
+    let result = processor("9701::<487297403495720912>");
+    expect(result.rawData).not.toBeUndefined();
+  });
+  test("returns -1 for rawData if missing < at position 0", function() {
+    let result = processor("9701::487297403495720912>");
+    expect(result.rawData).toBe(-1);
+  });
+  test("returns -1 for rawData if missing < at position -1", function() {
+    let result = processor("9701::<487297403495720912");
+    expect(result.rawData).toBe(-1);
+  });
+
+  test("returns -1 for rawData if missing both <> at position 0 and - 1", function() {
+    let result = processor("9701::487297403495720912");
+    expect(result.rawData).toBe(-1);
+  });
+
+  test("returns -1 for rawData if < is at a position other than 0", function() {
+    let result = processor("9701::4<87297403495720912>");
+    expect(result.rawData).toBe(-1);
+  });
+
+  test("returns -1 for rawData if > is at a position other than -1", function() {
+    let result = processor("9701::<48729740349572091>2");
+    expect(result.rawData).toBe(-1);
+  });
+
+  test("returns -1 for rawData if > is at a position other than -1", function() {
+    let result = processor("9701::<<48729740349572091>2>");
+    expect(result.rawData).toBe(-1);
+  });
+
  });
